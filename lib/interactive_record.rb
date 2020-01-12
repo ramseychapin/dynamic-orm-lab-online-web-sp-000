86346ca041f4b87aa45ddf277920e22014268176
Ramsey Chapin <ramsey@ramseychapin.com>
2020-01-12 02:21:55 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -59,7 +59,7 @@ class InteractiveRecord
     column_name = attribute.key
     value_name = attribute.value
     sql = "SELECT * FROM #{self.table_name} WHERE #{column_name} = ?"
-    DB[:conn].execute(sql, attribute)
+    DB[:conn].execute(sql, value_name)
   end
 
 end
