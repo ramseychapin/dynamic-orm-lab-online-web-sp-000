268b97e8ddaf040c06f0e86f464e91ce1f70c2e6
Ramsey Chapin <ramsey@ramseychapin.com>
2020-01-12 00:19:39 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,9 +2,9 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  self.table_name
-    self.to_s.downcase.pluralize
-  end
+  # self.table_name
+  #   self.to_s.downcase.pluralize
+  # end
 
   # def self.column_names
   #   DB[:conn].results_as_hash = true
