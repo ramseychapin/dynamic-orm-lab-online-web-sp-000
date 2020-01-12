47df31ccd92bd6b99414fa350d81ff11b7183965
Ramsey Chapin <ramsey@ramseychapin.com>
2020-01-12 00:21:14 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -14,10 +14,11 @@ class InteractiveRecord
 
     table_info = DB[:conn].execute(sql)
     column_names = []
-    
+
     table_info.each do |row|
       column_names << row["name"]
     end
+    
     column_names.compact
   end
 
