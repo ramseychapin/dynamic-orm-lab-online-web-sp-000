089e35fd6ac68c0ab75f88ffeea61045973019a6
Ramsey Chapin <ramsey@ramseychapin.com>
2020-01-12 00:20:11 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,7 +2,8 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  self.table_name
+
+  def self.table_name
     self.to_s.downcase.pluralize
   end
 
