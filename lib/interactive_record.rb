25bed9540f06103464f861903995137d4796354d
Ramsey Chapin <ramsey@ramseychapin.com>
2020-01-12 02:21:45 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -57,7 +57,7 @@ class InteractiveRecord
 
   def self.find_by(attribute)
     column_name = attribute.key
-    value name = attribute.value
+    value_name = attribute.value
     sql = "SELECT * FROM #{self.table_name} WHERE #{attribute} = ?"
     DB[:conn].execute(sql, attribute)
   end
