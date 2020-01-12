7c57dc43c487b3417405ecd18aada72bddde8eb3
Ramsey Chapin <ramsey@ramseychapin.com>
2020-01-12 02:04:09 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -32,6 +32,10 @@ class InteractiveRecord
     self.class.table_name
   end
 
+  def col_names_for_insert
+
+  end
+
   def values_for_insert
     values = []
     self.class.column_names.each do |col_name|
